e39b0b5479c9518e7ce261606b194bb9c154a643
Damian Mullins <damian@lowflyingowls.co.uk>
2017-09-22 10:42:25 +0100

    Adding JS behaviour and CSS styles for the navigation to help improve accessibility.
    sdfd
    
    sdf

---
+++ src/js/index.js
@@ -0,0 +1,57 @@
+/**
+ * @overview Fozzie header JavaScript behaviour.
+ *
+ * @module f-header
+ */
+
+import ready from 'lite-ready';
+
+
+const convertInputToButton = selector => {
+    const input = document.querySelector(selector);
+
+    if (input) {
+        const replaceTag = input.outerHTML.replace(/^<input/, '<button');
+
+        input.outerHTML = `${replaceTag}</button>`;
+
+        // Query the DOM again for this element now it has changed to a button
+        return document.querySelector(selector);
+    }
+};
+
+/**
+ * Setup the behaviour for the header component.
+ */
+const setupHeader = () => {
+    const menuButton = convertInputToButton('[data-nav-enhance]');
+
+    if (menuButton) {
+        /**
+         * Attach click event handler — as this element is now a button this event will
+         * trigger when the `enter` and `spacebar` keys are pressed.
+         *
+         * @see {@link https://www.w3.org/TR/html51/editing.html#running-synthetic-click-activation-steps - synthetic click activation steps}
+         */
+        menuButton.addEventListener('click', () => {
+            const navContainer = document.querySelector('[data-nav-container]');
+            const navLabel = document.querySelector('[data-nav-toggle]');
+
+            if (navContainer) {
+                navContainer.classList.toggle('is-visible');
+            }
+
+            if (navLabel) {
+                navLabel.classList.toggle('is-open');
+            }
+        });
+    }
+};
+
+ready(() => {
+
+    setupHeader();
+
+});
+
+export default setupHeader;
